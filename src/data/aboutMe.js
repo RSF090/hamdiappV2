f137a5f0fde6ad8db882d044e1bcd0c85908d83f
RSF090 <rsfurkan09@gmail.com>
2025-08-08 10:40:08 +0300

    title güncellendi

--- src/data/aboutMe.js
+++ src/data/aboutMe.js
@@ -1,4 +1,4 @@
 export const aboutMe = {
   title: 'Hakkımda',
-  content: 'Merhaba! Ben Hamdi Özbay, profesyonel bir podolog olarak ayak sağlığı alanında 5 yıldır hizmet veriyorum. Tırnak batması, nasır tedavisi, mantar enfeksiyonu ve diyabetik ayak bakımı konularında uzmanlaştım. Amacım, ayak sağlığını korumak ve insanların yaşam kalitesini artırmaktır. Bu blogda podolojiye dair tecrübelerimi ve önerilerimi paylaşıyorum.'
+  content: 'Merhaba! Ben Hamdi Özbay, profesyonel bir podolog olarak ayak sağlığı alanında 3 yıldır hizmet veriyorum. Tırnak batması, nasır tedavisi, mantar enfeksiyonu ve diyabetik ayak bakımı konularında uzmanlaştım. Amacım, ayak sağlığını korumak ve insanların yaşam kalitesini artırmaktır. Bu blogda podolojiye dair tecrübelerimi ve önerilerimi paylaşıyorum.'
 };
